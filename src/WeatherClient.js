09cbb5f73aa83286c2a01855010dd479151cd396
Naveen pandian <npandian515@gmail.com>
2025-08-13 21:10:07 +0530

    explited the joy of sdk creation with more class attributes

--- src/WeatherClient.js
+++ src/WeatherClient.js
@@ -11,5 +11,20 @@ const {
         if(!apikey || typeof apikey != 'string') {
             throw new InvalidApiKeyError();
         }
+
+
+        this.apikey = apikey;
+
+        this.config = {
+            baseUrl: options.baseUrl || 'https://api.weatherapi.com/v1',
+            timeout: options.timeout || 5000,
+            language : options.language || 'en',
+            debug : options.debug || false
+
+        }
+
+        this._log("SDK initialized")
     }
+
+    
  }
