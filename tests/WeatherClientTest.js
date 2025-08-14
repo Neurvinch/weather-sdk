6593034edc4dc25ea39f6b467ee4189b949e599e
Naveen pandian <npandian515@gmail.com>
2025-08-14 10:50:20 +0530

    test with jest line describing

---
+++ tests/WeatherClientTest.js
@@ -0,0 +1,14 @@
+const WeatherClient = require("../src/WeatherClient")
+
+const axios = require('axios')
+
+//  this make our jest thing to fully use the control of axios behavior
+
+jest.mock('axios');
+
+describe('WeatherClient', () => {
+    let client;
+
+    
+})
+
