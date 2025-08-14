59f0c4cd69fb2f01de87d38fe24d45e68ac67deb
Naveen pandian <npandian515@gmail.com>
2025-08-14 10:52:52 +0530

    lets us the describing keyword to instance weatherClient

--- tests/WeatherClientTest.js
+++ tests/WeatherClientTest.js
@@ -6,9 +6,17 @@ const axios = require('axios')
 
 jest.mock('axios');
 
+
+// create a client before use and clear before axios mocks
 describe('WeatherClient', () => {
     let client;
-
     
-})
+    beforeEach( () => {
+        client = new WeatherClient('test-api-key')
+        axios.mockClear()
+    })
+
+});
+
+
 
